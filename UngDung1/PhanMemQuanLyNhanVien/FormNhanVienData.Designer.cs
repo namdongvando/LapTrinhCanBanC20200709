c1198ab8e2fe0b144fa096124e36f3a64a51b551
namdongvando <32663464+namdongvando@users.noreply.github.com>
2020-08-06 18:08:42 +0700

    KeyNoiData

---
+++ UngDung1/PhanMemQuanLyNhanVien/FormNhanVienData.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PhanMemQuanLyNhanVien
+{
+    partial class FormNhanVienData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDSNhanVien = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSNhanVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDSNhanVien
+            // 
+            this.dgvDSNhanVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDSNhanVien.Location = new System.Drawing.Point(12, 84);
+            this.dgvDSNhanVien.Name = "dgvDSNhanVien";
+            this.dgvDSNhanVien.Size = new System.Drawing.Size(776, 354);
+            this.dgvDSNhanVien.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh Sách Nhân Viên";
+            // 
+            // FormNhanVienData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDSNhanVien);
+            this.Name = "FormNhanVienData";
+            this.Text = "FormNhanVienData";
+            this.Load += new System.EventHandler(this.FormNhanVienData_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSNhanVien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDSNhanVien;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ UngDung1/PhanMemQuanLyNhanVien/FormNhanVienData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhanMemQuanLyNhanVien
+{
+    public partial class FormNhanVienData : Form
+    {
+        private string ConnectStr = "Data Source=DESKTOP-M2PUTJR;Initial Catalog=QLHD;Integrated Security=True";
+
+        public FormNhanVienData()
+        {
+            InitializeComponent();
+        }
+
+        private void FormNhanVienData_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(ConnectStr);
+                SqlDataAdapter daKhachHang = new SqlDataAdapter("select * from nhanvien", conn);
+                DataTable dtKhachHang = new DataTable();
+                daKhachHang.Fill(dtKhachHang);
+                dgvDSNhanVien.DataSource = dtKhachHang;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
